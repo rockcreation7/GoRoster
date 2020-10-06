e4ca7a2b66fb3ea479ec3835135d30862fb2b28d
rock <rockcreation7@gmail.com>
2020-10-06 22:48:50 +0800

    update front end

--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"fmt"
 	"roster-api/middleware"
 
 	"github.com/gofiber/fiber/v2"
@@ -23,6 +24,16 @@ func Route(app *fiber.App) {
 	productAPI.Put("/update/:id", middleware.UpdateProduct)
 	productAPI.Get("/get/:id", middleware.GetProduct)
 	productAPI.Delete("/delete/:id", middleware.DeleteProduct)
+
+	app.Post("/image", func(c *fiber.Ctx) error {
+		// Get first file from form field "document":
+		file, err := c.FormFile("document")
+		if err != nil {
+			return err
+		}
+		// Save file to root directory:
+		return c.SaveFile(file, fmt.Sprintf("./dist/img/%s", file.Filename))
+	})
 
 	app.Static("/", "dist")
 	app.Get("/*", func(c *fiber.Ctx) error {
