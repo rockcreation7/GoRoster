b815109a37e4ecf95f085f0fbaf9fde62ff2654d
Rock <rockcreation7@gmail.com>
2020-11-11 15:32:22 +0800

    add table check

--- db/db.go
+++ db/db.go
@@ -24,7 +24,10 @@ func Connect() error {
 	}
 
 	Dbconnect, err = sql.Open("postgres", os.Getenv("POSTGRES_URL"))
-	migrate(Dbconnect)
+	isTableExist := checkIfTableExist(Dbconnect)
+	if !isTableExist {
+		migrate(Dbconnect)
+	}
 
 	if err != nil {
 		panic(err)
@@ -39,17 +42,28 @@ func Connect() error {
 }
 
 func migrate(Dbconnect *sql.DB) {
-	var err error
-	// execute the sql statement
-	var stmt *sql.Stmt
-	stmt, err = Dbconnect.Prepare("CREATE Table product(id int NOT NULL, Imgurl varchar(50), catagory varchar(50),code int, price int, qty varchar(50), name varchar(50), PRIMARY KEY (id))")
+	stmt, err := Dbconnect.Prepare("CREATE Table products(id int NOT NULL, cost int, Imgurl varchar(50), catagory varchar(50),code int, price int, qty varchar(50), name varchar(50), PRIMARY KEY (id))")
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+
 	_, err = stmt.Exec()
+
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
 		fmt.Println("Table created successfully")
 	}
+}
+
+// stmt, err = Dbconnect.Prepare("ALTER TABLE products DROP email;")
+func checkIfTableExist(Dbconnect *sql.DB) bool {
+	_, tableCheck := Dbconnect.Query("select * from products;")
+	if tableCheck == nil {
+		// fmt.Println("table is there", res)
+		return true
+	}
+	// fmt.Println("table not there", res)
+	return false
 }
